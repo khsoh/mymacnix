3283d274b4ae99d964b20097a97675023bb6978c
K H Soh <hju37823@outlook.com>
2024-06-04 16:46:09 +0800

    Moved tmux update launch agent into nix

--- HOME/.config/nixpkgs/darwin/home.nix
+++ HOME/.config/nixpkgs/darwin/home.nix
@@ -1,11 +1,34 @@
 { config, pkgs, ... }: 
-{
+let
+  USERHOME = builtins.getEnv "HOME";
+in {
   ## Need to install nerdfonts here instead of nix-darwin's users.users
   ## because nix-darwin did not link the fonts to ~/Library/Fonts folder
   home.packages = with pkgs;
   [ 
     (nerdfonts.override { fonts = [ "FiraMono" ]; })
   ];
+
+  launchd.enable = true;
+  launchd.agents = {
+    updateTmuxPlugins = {
+      enable = true;
+      config = {
+        Label = "updateTmuxPlugins";
+        ProgramArguments = [ "/usr/bin/bash"
+          "-c"
+          "[ -d ${USERHOME}/.tmux/plugins/tpm ] || exit ;
+           for d in ${USERHOME}/.tmux/plugins/*; do
+             ${pkgs.git}/bin/git -C $d pull --recurse-submodules ;
+           done"
+          ];
+        RunAtLoad = true;
+        KeepAlive = { SuccessfulExit = false; };
+        StandardOutputPath = "${USERHOME}/log/tmuxupdate.log";
+        StandardErrorPath = "${USERHOME}/log/tmuxupdateError.log";
+      };
+    };
+  };
 
   # The state version is required and should stay at the version you
   # originally installed.
