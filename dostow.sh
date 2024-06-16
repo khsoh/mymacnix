54993df26c7f398f6131f1c44986a9dc0f57639e
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-06-16 15:28:28 +0800

    Updates to simplify dotfiles setup

--- dostow.sh
+++ dostow.sh
@@ -4,6 +4,6 @@ SCRIPTNAME=$(readlink -f ${BASH_SOURCE[0]})
 pushd "$(dirname $SCRIPTNAME)" > /dev/null
 
 # Sets up the symlinks with stow
-stow -Rt ~ HOME 2> >(grep -v "BUG in find_stowed_path" 1>&2)
+stow -Rt ~/.config dot-config 2> >(grep -v "BUG in find_stowed_path" 1>&2)
 
 popd >/dev/null
