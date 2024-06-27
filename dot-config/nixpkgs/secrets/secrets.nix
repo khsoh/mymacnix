87bebae1fea8b943123706dc552765faa61027b1
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-06-27 17:28:38 +0800

    Changed secrets.nix to include assertions and warnings for the modules

--- dot-config/nixpkgs/secrets/secrets.nix
+++ dot-config/nixpkgs/secrets/secrets.nix
@@ -2,6 +2,7 @@ let
   pkgs = import <nixpkgs> {};
   sshcfg = (pkgs.lib.evalModules {
     modules = [
+      <nixpkgs/nixos/modules/misc/assertions.nix>
       ./../darwin/cfg.nix
     ];
   }).config.mod_sshkeys;
