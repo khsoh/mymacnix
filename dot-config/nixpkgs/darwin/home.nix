bcd629c2b8423bddf87dc87f149c1238ab4416d8
K H Soh <hju37823@outlook.com>
2024-06-28 21:54:44 +0800

    Moved the tmux startup from external script into a launch agent

--- dot-config/nixpkgs/darwin/home.nix
+++ dot-config/nixpkgs/darwin/home.nix
@@ -103,7 +103,18 @@ in {
       enable = true;
       config = {
         Label = "LoginStartTmux";
-        ProgramArguments = [ "${syscfg.HOME}/.config/tmux/osx_tmux_terminal_startup.sh" ];
+        ProgramArguments = [
+          "osascript"
+          "-e" "
+          tell application \"Terminal\"
+            if not (exists window 1) then reopen
+            activate
+            set winID to id of window 1
+            do script \"tmux 2>/dev/null\" in window id winID
+          end tell
+          return
+          "
+          ];
         RunAtLoad = true;
       };
     };
