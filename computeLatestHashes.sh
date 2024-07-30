55836d222d5d33e5172b563d80ab8030e35f3bea
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-07-30 10:29:02 +0800

    Added script to get the latest commit hash and compute the SHA256 SRI checksum for repos referenced in home.nix

---
+++ computeLatestHashes.sh
@@ -0,0 +1,14 @@
+#!/run/current-system/sw/bin/zsh
+
+## This script gets the latest commit hash of the main/master branch of the repos
+## referenced in home.nix and computes the SRI SHA256 checksum for each repo
+declare -a REPOS=()
+REPOS+=("https://github.com/khsoh/kickstart.nvim;refs/heads/master")
+REPOS+=("https://github.com/khsoh/tmuxconf;refs/heads/master")
+
+for repo in "${REPOS[@]}";  do
+    IFS=";"; read url ref <<< $repo
+    rev=$(git ls-remote $url $ref|awk '{print $1}')
+    hash=$(nix-hash --to-sri --type sha256 $(nix-prefetch-url --unpack $url/archive/${rev}.tar.gz 2> /dev/null))
+    printf "REPO=$url\nREV=$rev\nSHA256=$hash\n\n"
+done
