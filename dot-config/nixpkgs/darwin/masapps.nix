670bcf9871584eba70ce2cfe15ab320341dacf31
K H Soh <2169449+khsoh@users.noreply.github.com>
2024-08-04 21:02:40 +0800

    Updated to brews.nix to support machine specific installations of casks and masapps

---
+++ dot-config/nixpkgs/darwin/masapps.nix
@@ -0,0 +1,5 @@
+{
+  ## Refer to https://daiderd.com/nix-darwin/manual/index.html#opt-homebrew.masApps for the format of the attribute
+  ## Requires user to signin to App Store
+  #  "WhatsApp" = 310633997;
+}
