dbabb116567f0c9f8d4f77770fa2731b510769db
K H Soh <hju37823@outlook.com>
2024-09-13 12:09:09 +0800

    Fixed programs.ssh config

--- dot-config/nixpkgs/darwin/home.nix
+++ dot-config/nixpkgs/darwin/home.nix
@@ -117,22 +117,19 @@ in {
   };
 
   ### Enable ssh configuration
-  programs.ssh = {
-    enable = true;
-    addKeysToAgent = if onepasscfg.sshsign_pgm_present then "no" else "yes";
-
-    matchBlocks = {
-      #user = { signingkey = if onepasscfg.sshsign_pgm_present then sshcfg.userssh_pubkey else sshcfg.nixidssh_pubkey; };
-      "*" = if onepasscfg.sshsign_pgm_present then {
-              extraOptions = {
-                  IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
-                };
-              }
-            else {
-              identityFile = sshcfg.NIXIDPKFILE;
-            };
+  programs.ssh = if onepasscfg.sshsign_pgm_present then
+    {
+      enable = true;
+      addKeysToAgent = "no";
+      extraConfig = ''
+        IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+      '';
+    } else {
+      enable = true;
+      extraConfig = ''
+        IdentityFile = sshcfg.NIXIDPKFILE;
+      '';
     };
-  };
 
   ### Enable git configuration
   programs.git = {
