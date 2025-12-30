5fc75e1f0b870dded2cf8947957a466230c6a0ca
K H Soh <hju37823@outlook.com>
2025-12-30 11:28:42 +0800

    Converted computeLatestHashes.sh to read repos to compute from home.nix

--- computeLatestHashes.sh
+++ computeLatestHashes.sh
@@ -2,15 +2,20 @@
 
 ## This script gets the latest commit hash of the main/master branch of the repos
 ## referenced in home.nix and computes the SRI SHA256 checksum for each repo
-declare -a REPOS=()
-REPOS+=("https://github.com/khsoh/kickstart.nvim;refs/heads/master")
-REPOS+=("https://github.com/khsoh/tmuxconf;refs/heads/master")
-REPOS+=("https://github.com/khsoh/kittyconf;refs/heads/main")
-REPOS+=("https://github.com/khsoh/gttyconf;refs/heads/main")
 
-for repo in "${REPOS[@]}";  do
-    IFS=";"; read url ref <<< $repo
-    rev=$(git ls-remote $url $ref|awk '{print $1}')
-    hash=$(nix --experimental-features nix-command hash convert --hash-algo sha256 --to sri $(nix-prefetch-url --unpack $url/archive/${rev}.tar.gz 2> /dev/null))
-    printf "REPO=$url\nrev=\"$rev\";\nsha256=\"$hash\";\n\n"
+nix-instantiate --eval --json --strict -E "
+  let
+    config = (import <darwin> { }).config;
+    files = config.home-manager.users.\"$(id -un)\".home.file;
+  in
+    builtins.mapAttrs (name: value: {
+      owner = value.source.owner or null;
+      repo = value.source.repo or null;
+    }) files
+" | jq -r 'to_entries[] | select(.value.repo != null) | "https://github.com/\(.value.owner)/\(.value.repo)"' \
+    | while IFS= read -r repo; do
+    rev=$(git ls-remote $repo HEAD|awk '{print $1}')
+    hash=$(nix --experimental-features nix-command hash convert --hash-algo sha256 --to sri $(nix-prefetch-url --unpack $repo/archive/${rev}.tar.gz 2> /dev/null))
+    printf "REPO=$repo\nrev=\"$rev\";\nsha256=\"$hash\";\n\n"
 done
+
