56188f497992eb6268b6a4e19815b725a2cd03e7
K H Soh <hju37823@outlook.com>
2025-12-30 18:08:18 +0800

    Simplified computeLatestHashes.sh to not use jq

--- computeLatestHashes.sh
+++ computeLatestHashes.sh
@@ -3,17 +3,20 @@
 ## This script gets the latest commit hash of the main/master branch of the repos
 ## referenced in home.nix and computes the SRI SHA256 checksum for each repo
 
-nix-instantiate --eval --json --strict -E "
+nix-instantiate --eval --json --strict -E --raw "
   let
     config = (import <darwin> { }).config;
-    files = config.home-manager.users.\"$(id -un)\".home.file;
+    lib = (import <nixpkgs> { }).lib;
+    _files = config.home-manager.users.\"$(id -un)\".home.file;
+    # Get only fetchFromGitHub files
+    ghfiles = lib.attrsets.filterAttrs (n: v: v.source ? githubBase) _files;
   in
-    builtins.mapAttrs (name: value: {
-      owner = value.source.owner or null;
-      repo = value.source.repo or null;
-    }) files
-" | jq -r 'to_entries[] | select(.value.repo != null) | "https://github.com/\(.value.owner)/\(.value.repo)"' \
-    | while IFS= read -r repo; do
+    lib.concatLines (lib.attrsets.mapAttrsToList (name: value:  \"https://\" +
+      value.source.githubBase + \"/\" +
+      value.source.owner + \"/\" +
+      value.source.repo
+      ) ghfiles)
+" | while IFS= read -r repo; do
     rev=$(git ls-remote $repo HEAD|awk '{print $1}')
     hash=$(nix --experimental-features nix-command hash convert --hash-algo sha256 --to sri $(nix-prefetch-url --unpack $repo/archive/${rev}.tar.gz 2> /dev/null))
     printf "REPO=$repo\nrev=\"$rev\";\nsha256=\"$hash\";\n\n"
