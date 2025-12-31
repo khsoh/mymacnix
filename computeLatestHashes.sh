bf1c4305816829da69ec741b53c19b0c813d8cf8
K H Soh <hju37823@outlook.com>
2025-12-31 11:50:39 +0800

    Minor simplifications to computeLatestHashes.sh

--- computeLatestHashes.sh
+++ computeLatestHashes.sh
@@ -1,9 +1,10 @@
-#!/run/current-system/sw/bin/zsh
+#!/usr/bin/env zsh
 
-## This script gets the latest commit hash of the main/master branch of the repos
+## This script gets the latest commit hash of the HEAD at the main/master of the repos
 ## referenced in home.nix and computes the SRI SHA256 checksum for each repo
+## The outputs can then be used to replace the corresponding entries in the home.nix file
 
-nix-instantiate --eval --json --strict -E --raw "
+nix-instantiate --eval --json -E --raw "
   let
     config = (import <darwin> { }).config;
     lib = (import <nixpkgs> { }).lib;
