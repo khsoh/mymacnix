4182900f69bb79523abaf620a9eab761166b69b3
K H Soh <hju37823@outlook.com>
2026-02-24 13:35:53 +0800

    Added terminal.nix configuration file

---
+++ dot-config/nixpkgs/darwin/usermod/terminal.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  osConfig,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  isVM = osConfig.machineInfo.is_vm;
+in
+{
+  ## Terminal program for user
+  options.terminal = {
+    package = lib.mkPackageOption pkgs [ "ghostty-bin" "kitty" ] {
+      nullable = true;
+      default = null;
+      extraDescription = "Terminal package to install";
+    };
+  };
+
+  ## Setup checks and asserts for the SSH private and public key files based on
+  ## user configuration
+  config.assertions = [
+    {
+      assertion = (!isVM) || (config.terminal.package != pkgs.kitty);
+      message = "kitty terminal program cannot be installed in a VM because VM does not support OpenGL drivers";
+    }
+  ];
+}
+# vim: set ts=2 sw=2 et ft=nix:
