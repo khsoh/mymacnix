ae2ae0469e8e91569ca3700dfd5e8eb189b69975
K H Soh <hju37823@outlook.com>
2026-03-06 19:19:31 +0800

    Improved deployment code

--- installScripts/scpdeploy.sh
+++ installScripts/scpdeploy.sh
@@ -1,19 +1,38 @@
 #!/usr/bin/env zsh
 
+set -u
+set -o pipefail
+
+nixtmpDir="$(mktemp -d -t scpdeploydir.XXXXXXXXX || \
+          echo "Can't create temporary directory for deploying secrets remotely")"
+
+cleanup() {
+    rm -rf "$nixtmpDir"
+}
+
+trap cleanup EXIT INT TERM QUIT
+
 echo "Copying deploy.map and token..."
 
 read "REMOTE_HOST?SSH destination in <user>@<ipaddr> form: "
 
 read "DEPLOY_FILE?Name of local deploy.map file to copy to remote host: "
 
+# Replace leading ~ with $HOME
+DEPLOY_FILE="${DEPLOY_FILE/#\~/$HOME}"
+
 if [ ! -f "$DEPLOY_FILE" ]; then
     echo "ERROR: Deploy map file $DEPLOY_FILE does not exist"
     exit 1
 fi
 
+## Create a dummy known_hosts file
+KNOWN_HOSTS="$nixtmpDir/known_hosts"
+touch $KNOWN_HOSTS
+
 ## Copy over the file
 echo "Copying file to remote host"
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "$DEPLOY_FILE" $REMOTE_HOST:~/.deploy/deploy.map
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=$KNOWN_HOSTS "$DEPLOY_FILE" $REMOTE_HOST:~/.deploy/deploy.map
 
 
 echo "Enter vault names one by one.  Press ENTER on an empty line when finished:"
@@ -42,4 +61,4 @@ echo "Generating token for vaults: ${VAULTS[*]}..."
 # Generate the unique name
 SA_NAME="tmp-$(date +%Y%m%d-%H%M)-$(uuidgen | head -c 8)"
 
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $REMOTE_HOST "cat > ~/.deploy/token" < <(op service-account create "$SA_NAME" "${VAULTS[@]}" --expires-in=10m --raw)
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=$KNOWN_HOSTS $REMOTE_HOST "cat > ~/.deploy/token" < <(op service-account create "$SA_NAME" "${VAULTS[@]}" --expires-in=10m --raw)
