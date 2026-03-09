979dfcb8a240e1f4885f727cc2329f8cb402a816
K H Soh <hju37823@outlook.com>
2026-03-09 17:51:59 +0800

    Updated to support local host deployment

--- installScripts/deploySecrets.sh
+++ installScripts/deploySecrets.sh
@@ -48,13 +48,24 @@ trap cleanup EXIT INT TERM QUIT
 
 function usage () {
     print_green "Usage: "
-    print_green "  $1 <user> <host>"
+    print_green "  $1 [<user> <host>]"
     print_green " <user> : Username - should be one folder names under ../dot-config/nixpkgs/secrets/user"
     print_green " <host> : Host - should be one folder names under ../dot-config/nixpkgs/secrets/host"
+    print_green " If no arguments are provided, scripts are generated at ~/.deploy to deploy the secrets at the local host and user"
     exit 1
 }
 
-if [ "$#" -ne 2 ]; then
+INSTALL_REMOTE=1
+if [ "$#" -eq 2 ]; then
+    XUSER=$1
+    XHOST=$2
+elif [ "$#" -eq 0 ]; then
+    INSTALL_REMOTE=""
+    XHOST="$(/usr/sbin/scutil --get LocalHostName)"
+    XUSER=$(nix-instantiate --eval --json --strict -E '(import (<darwin-config> + "/../secrets/getpkinfo.nix")).pkuser.name')
+    XUSER="${XUSER%\"}" # Remove trailing quote
+    XUSER="${XUSER#\"}" # Remove leading quote
+else
     usage $0
 fi
 
@@ -62,25 +73,25 @@ fi
 DARWINDIR=$(nix-instantiate --eval -E '<darwin-config>')
 USERDIR=$(readlink -f "${DARWINDIR}/../secrets/user")
 HOSTDIR=$(readlink -f "${DARWINDIR}/../secrets/host")
-goodargs=true
-if [[ ! -d "$USERDIR/$1" ]]; then
-    print_red "ERROR: $1 does not exist in $USERDIR"
+goodargs=1
+if [[ ! -d "$USERDIR/$XUSER" ]]; then
+    print_red "ERROR: $XUSER does not exist in $USERDIR"
     print_red "Available users:"
     print_green "$(ls $USERDIR)"
-    goodargs=false
+    goodargs=""
 fi
 
-if [[ ! -d "$HOSTDIR/$2" ]]; then
-    print_red "ERROR: $2 does not exist in $HOSTDIR"
+if [[ ! -d "$HOSTDIR/$XHOST" ]]; then
+    print_red "ERROR: $XHOST does not exist in $HOSTDIR"
     print_red "Available hosts:"
     print_green "$(ls $HOSTDIR)"
-    goodargs=false
+    goodargs=""
 fi
 
-$goodargs || exit 1
+[ -n "$goodargs" ] || exit 1
 
-print_green "Deploying from user: ${USERDIR}/$1"
-print_green "Deploying from host: ${HOSTDIR}/$1"
+print_green "Deploying from user: ${USERDIR}/$XUSER"
+print_green "Deploying from host: ${HOSTDIR}/$XHOST"
 
 PKDATA=$(nix-instantiate --eval --strict --json - <<EOF
 let
@@ -88,8 +99,8 @@ let
   pkdata = import (base + "/../secrets/getpkinfo.nix");
 in
 {
-  pkuser = pkdata.users."$1";
-  pkhost = pkdata.hosts."$2";
+  pkuser = pkdata.users."$XUSER";
+  pkhost = pkdata.hosts."$XHOST";
 }
 EOF
 )
@@ -114,24 +125,36 @@ sudo -E -s <<'EOF'
 EOFX
 )
 
-vared -p "SSH destination in <user>@<ipaddr> form: " -c REMOTE_HOST
+function run() {
+    if [ -n "$INSTALL_REMOTE" ]; then
+        ssh -S "$SOCKET" "$REMOTE_HOST" "zsh -c '$1'"
+    else
+        zsh -c "$1"
+    fi
+}
 
-SOCKET="$HOME/ssh_mux_$$.sock"
-print_green "Setting up SSH ControlMaster connection to $REMOTE_HOST in background"
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -fNM -S "$SOCKET" "$REMOTE_HOST"
+if [ -n "$INSTALL_REMOTE" ]; then
+    ## Install remotely
+    vared -p "SSH destination in <user>@<ipaddr> form: " -c REMOTE_HOST
+
+    SOCKET="$HOME/ssh_mux_$$.sock"
+    print_green "Setting up SSH ControlMaster connection to $REMOTE_HOST in background"
+    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -fNM -S "$SOCKET" "$REMOTE_HOST"
+fi
 
 ## Create a clean deployment directory in host
-print_green "Creating clean deployment directory in ~/.deploy at $REMOTE_HOST"
-ssh -S "$SOCKET" "$REMOTE_HOST" "rm -rf ~/.deploy && mkdir -p ~/.deploy/root"
+print_green "Creating clean deployment directory in ~/.deploy at $XHOST"
+run "rm -rf ~/.deploy && mkdir -p ~/.deploy/root"
 
-## Copy over the user files
+
+## Copy the user secrets
 echo $PKDATA | jq '.pkuser.DEPLOY' | jq -c '.[]' | while read -r item; do
   opuri=$(echo "$item" | jq -r ".OPURI")
   file=$(echo "$item" | jq -r ".FILE")
   cmds=$(echo "$item" | jq -r ".POSTCMD" | jq -r '.[]')
 
   # Get the secret into the remote host destination
-  op read "$opuri" | ssh -S "$SOCKET" "$REMOTE_HOST" "mkdir -p \$(dirname $file) && umask 077 && cat > $file"
+  op read "$opuri" | run "mkdir -p \$(dirname $file) && umask 077 && cat > $file"
   USERCMDS=$(cat <<EOF
 $USERCMDS
 $cmds
@@ -140,16 +163,16 @@ EOF
 )
 done
 USERCMDS="$USERCMDS\npopd\n"
-echo "$USERCMDS" | ssh -S "$SOCKET" "$REMOTE_HOST" "cat > ~/.deploy/userdeploy.sh && chmod +x ~/.deploy/userdeploy.sh"
+echo "$USERCMDS" | run "cat > ~/.deploy/userdeploy.sh && chmod +x ~/.deploy/userdeploy.sh"
 
-## Copy over the root files
+## Copy the host secrets
 echo $PKDATA | jq '.pkhost.DEPLOY' | jq -c '.[]' | while read -r item; do
   opuri=$(echo "$item" | jq -r ".OPURI")
   file=$(echo "$item" | jq -r ".FILE")
   cmds=$(echo "$item" | jq -r ".POSTCMD" | jq -r '.[]')
 
   # Get the secret into the remote host destination
-  op read "$opuri" | ssh -S "$SOCKET" "$REMOTE_HOST" "mkdir -p \$(dirname ~/.deploy/root/$file) && umask 077 && cat > ~/.deploy/root/$file"
+  op read "$opuri" | run "mkdir -p \$(dirname ~/.deploy/root/$file) && umask 077 && cat > ~/.deploy/root/$file"
   HOSTCMDS=$(cat <<EOF
 $HOSTCMDS
 mkdir -p \$(dirname $file)
@@ -159,7 +182,9 @@ EOF
 )
 done
 HOSTCMDS="$HOSTCMDS\nEOF\npopd\n"
-echo "$HOSTCMDS" | ssh -S "$SOCKET" "$REMOTE_HOST" "cat > ~/.deploy/hostdeploy.sh && chmod +x ~/.deploy/hostdeploy.sh && touch ~/.deploy/completed"
+echo "$HOSTCMDS" | run "cat > ~/.deploy/hostdeploy.sh && chmod +x ~/.deploy/hostdeploy.sh && touch ~/.deploy/completed"
 
-print_green "Completed transfer - closing SSH ControlMaster connection"
-ssh -S "$SOCKET" -O exit "$REMOTE_HOST"
+if [ -n "$INSTALL_REMOTE" ]; then
+    print_green "Completed transfer - closing SSH ControlMaster connection"
+    ssh -S "$SOCKET" -O exit "$REMOTE_HOST"
+fi
