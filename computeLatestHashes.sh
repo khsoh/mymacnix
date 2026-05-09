1a684d6c2aede141f309314b61a2dd23a44639bf
K H Soh <hju37823@outlook.com>
2026-05-09 16:56:13 +0800

    Minor whitespace fix for computeLatestHashes.sh

--- computeLatestHashes.sh
+++ computeLatestHashes.sh
@@ -5,9 +5,9 @@
 ## The outputs can then be used to replace the corresponding entries in the home.nix file
 
 function cleanup() {
-  if [ -n "$ULIMIT" ]; then
-    ulimit -n $ULIMIT
-  fi
+    if [ -n "$ULIMIT" ]; then
+        ulimit -n $ULIMIT
+    fi
 }
 
 trap cleanup EXIT INT TERM QUIT
@@ -15,22 +15,22 @@ trap cleanup EXIT INT TERM QUIT
 ULIMIT=$(ulimit -n)
 ulimit -n 4096
 nix-instantiate --eval --json -E --raw "
-  let
-    config = (import <darwin> { }).config;
-    lib = (import <nixpkgs> { }).lib;
-    _files = config.home-manager.users.\"$(id -un)\".home.file;
-    # Get only fetchFromGitHub files
-    ghfiles = lib.attrsets.filterAttrs (n: v: v.source ? githubBase) _files;
-  in
-    lib.concatLines (lib.attrsets.mapAttrsToList (name: value:
-      \"\${value.source.owner}:\${value.source.repo}\"
-      ) ghfiles)
+    let
+        config = (import <darwin> { }).config;
+        lib = (import <nixpkgs> { }).lib;
+        _files = config.home-manager.users.\"$(id -un)\".home.file;
+        # Get only fetchFromGitHub files
+        ghfiles = lib.attrsets.filterAttrs (n: v: v.source ? githubBase) _files;
+    in
+        lib.concatLines (lib.attrsets.mapAttrsToList (name: value:
+            \"\${value.source.owner}:\${value.source.repo}\"
+        ) ghfiles)
 " | while IFS=':' read -r owner repo; do
-  # Skip empty lines or lines that do not match the format
-  [[ -z "$owner" || -z "$repo" ]] && continue
+    # Skip empty lines or lines that do not match the format
+    [[ -z "$owner" || -z "$repo" ]] && continue
 
-  ## Run the alias for this command
-  # nix-prefetch-github --nix $owner $repo | tail -n +4 | sed -e 's/pkgs.fetchFromGitHub {/{/'
-  nx-pfgh $owner $repo
+    ## Run the alias for this command
+    # nix-prefetch-github --nix $owner $repo | tail -n +4 | sed -e 's/pkgs.fetchFromGitHub {/{/'
+    nx-pfgh $owner $repo
 done
 
