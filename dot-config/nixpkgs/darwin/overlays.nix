606a76e5c51b67c6562cc09de1baf80a654bd151
K H Soh <hju37823@outlook.com>
2026-05-28 10:04:35 +0800

    Updated bitwarden-desktop overlay ignoreCommits list

--- dot-config/nixpkgs/darwin/overlays.nix
+++ dot-config/nixpkgs/darwin/overlays.nix
@@ -7,6 +7,7 @@ let
       url = "https://github.com/NixOS/nixpkgs/archive/d99b013d5d19.tar.gz";
       ignoredCommits = [
         "f9d8b6595035"
+        "4100e830e085"
       ];
       # Optional: Add a description or version tag for clarity
       desc = "Pinned bitwarden-desktop";
