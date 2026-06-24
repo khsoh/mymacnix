af2078affa45607d90832add436e1e8e14413444
K H Soh <hju37823@outlook.com>
2026-06-24 11:26:41 +0800

    Updated postActivation code to only modify /etc/hosts file when there are changes

--- dot-config/nixpkgs/darwin/postActivation/addUserHosts.nix
+++ dot-config/nixpkgs/darwin/postActivation/addUserHosts.nix
@@ -23,27 +23,36 @@ in
           # bash
           ''
             # shellcheck disable=SC2059
-            printf "''${GREEN}''${BOLD}======== Private Host Mappings ========''${ESC}\n"
+            printf "''${GREEN}''${BOLD}======== User Host Mappings ========''${ESC}\n"
 
             # Nix will automatically evaluate this to your user's specific runtime path
             SECRET_PATH="${userHostsPath}"
 
-            if [ -f "$SECRET_PATH" ]; then
-              START_MARKER="# --- START AGENIX HOSTS ---"
-              END_MARKER="# --- END AGENIX HOSTS ---"
+            START_MARKER="# --- START AGENIX HOSTS ---"
+            END_MARKER="# --- END AGENIX HOSTS ---"
+
+            # Create the new block
+            NEW_BLOCK="$START_MARKER"$'\n'
+            [ -f "$SECRET_PATH" ] && NEW_BLOCK+=$(cat "$SECRET_PATH")$'\n' || true
+            NEW_BLOCK+="$END_MARKER"
+
+            # Extract the existing block and strip its blank lines
+            CURRENT_BLOCK=$(sed -n "/$START_MARKER/,/$END_MARKER/p" /etc/hosts | grep -v "^\s*$")
+
+            # Only edit /etc/hosts if the contents do not match
+            if [ "$NEW_BLOCK" != "$CURRENT_BLOCK" ]; then
+              # shellcheck disable=SC2059
+              printf "''${RED}''${BOLD}==>''${ESC} Modifying /etc/hosts file with user host mappings\n"
 
               # Root cleans out the old markers from /etc/hosts
               sed -i "" "/$START_MARKER/,/$END_MARKER/d" /etc/hosts
 
-              # Root reads your user-owned secret and appends it to /etc/hosts
-              {
-                echo "$START_MARKER"
-                cat "$SECRET_PATH"
-                echo "$END_MARKER"
-              } >> /etc/hosts
+              # Add in the new block
+              echo "$NEW_BLOCK" >> /etc/hosts
             fi
+
             # shellcheck disable=SC2059
-            printf "''${BLUE}''${BOLD}==>''${ESC} Completed Private Host Mappings\n"
+            printf "''${BLUE}''${BOLD}==>''${ESC} Completed User Host Mappings\n"
           ''
       );
 }
