dd1661bc5c5f82a45257314875770d43c409d743
K H Soh <hju37823@outlook.com>
2026-07-07 11:36:10 +0800

    Updated configuration.nix to workaround https://github.com/nix-darwin/nix-darwin/issues/1817

--- dot-config/nixpkgs/darwin/configuration.nix
+++ dot-config/nixpkgs/darwin/configuration.nix
@@ -629,6 +629,9 @@ in
     enable = !isVM;
   };
 
+  documentation.enable = false;
+  system.tools.darwin-uninstaller.enable = false;
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 5;
