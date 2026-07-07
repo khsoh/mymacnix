a16be498b3e46a56294e7a22b0386a215f7fd785
K H Soh <hju37823@outlook.com>
2026-07-07 16:04:53 +0800

    Removed workaround for the darwin manual build bug

--- dot-config/nixpkgs/darwin/configuration.nix
+++ dot-config/nixpkgs/darwin/configuration.nix
@@ -629,9 +629,6 @@ in
     enable = !isVM;
   };
 
-  documentation.enable = false;
-  system.tools.darwin-uninstaller.enable = false;
-
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 5;
