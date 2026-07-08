f007fdf86157263e0f9f68e2af7d117881f8eaa6
K H Soh <hju37823@outlook.com>
2026-07-08 11:42:28 +0800

    Updates to migrate use of ETag to github commit atom to detect nix channel updates

--- dot-config/nixpkgs/launchdagents/checkNixpkgs.sh
+++ dot-config/nixpkgs/launchdagents/checkNixpkgs.sh
@@ -23,20 +23,20 @@ is_running_under_login() {
     return 2 # '/usr/bin/login' not found in the hierarchy
 }
 
-get_etag() {
-    curl -sIL --connect-timeout 20 \
-        --retry 3 \
-        --retry-delay 2 \
-        --retry-connrefused \
-        "$1" | grep -i '^etag:' | tail -n1 | awk '{print $2}' | tr -d '\r\n"'
-}
-
 get_gitrevision() {
     curl -sIL --connect-timeout 20 \
         --retry 3 \
         --retry-delay 10 \
         --retry-connrefused \
         -o /dev/null -w '%{url_effective}' "$1" | sed -e 's/.*[\./]//'
+}
+
+get_atominfo() {
+    curl -sL --connect-timeout 20 \
+        --retry 3 \
+        --retry-delay 10 \
+        --retry-connrefused \
+        "$1" | awk '/<entry>/{flag=1} flag && /<id>/{print; exit}' | sed -E 's/.*Commit\///' | sed -e 's/<[^>]*>//g' | xargs
 }
 
 is_running_under_login
@@ -95,6 +95,7 @@ else
 fi
 
 declare -A NIXCHANNELS
+declare -A FEEDS
 
 while read -r name url; do
     # Skip empty lines or lines missing a name
@@ -102,12 +103,41 @@ while read -r name url; do
 
     # Assign the url to the name index
     NIXCHANNELS["$name"]="$url"
+
+    # Compute the feed
+    if [[ "$url" =~ github\.com ]]; then
+        # Clean URL and strip tracking prefixes/extensions
+        CLEAN_PATH=$(echo "$url" | sed -E 's|https://github.com/||' | sed 's|/archive/|/|')
+
+        OWNER=$(echo "$CLEAN_PATH" | cut -d'/' -f1)
+        REPO=$(echo "$CLEAN_PATH" | cut -d'/' -f2)
+
+        # Strip trailing package extensions (.tar.gz or .zip) from the branch segment
+        BRANCH=$(echo "$CLEAN_PATH" | cut -d'/' -f3 | sed -E 's/\.(tar\.gz|zip)//')
+
+        FEED_URL="https://github.com/${OWNER}/${REPO}/commits/${BRANCH}.atom"
+        FEEDS["$name"]="$FEED_URL"
+    elif [[ "$url" =~ nixos\.org ]]; then
+        # This regex isolates the trailing identifier (e.g., nixpkgs-unstable) regardless of the domain prefix
+        CHANNEL_NAME=$(echo "$url" | sed -E 's|https://(channels\.)?nixos\.org(/channels)?/||' | sed 's|/||g')
+
+        FEED_URL="https://github.com/NixOS/nixpkgs/commits/${CHANNEL_NAME}.atom"
+        FEEDS["$name"]="$FEED_URL"
+    else
+        printf "%s===> Channel %s: Unknown channel format - cannot derive feed%s\n" "${BOLD}${RED}" "$name" "${ESC}"
+    fi
 done < <(sudo -H nix-channel --list)
 
 LOCAL_NIXPKGSREVISION=$(darwin-version --nixpkgs-revision | tr -d '\r\n')
 
 # Get the git revision from the effective URL of the nixpkgs channel
-REMOTE_NIXPKGSREVISION=$(get_gitrevision "${NIXCHANNELS['nixpkgs']}")
+REMOTE_NIXPKGSREVISION=""
+if [[ -v FEEDS["nixpkgs"] ]]; then
+    REMOTE_NIXPKGSREVISION=$(get_atominfo "${FEEDS['nixpkgs']}")
+else
+    # This is slower than getting from feed
+    REMOTE_NIXPKGSREVISION=$(get_gitrevision "${NIXCHANNELS['nixpkgs']}")
+fi
 LOCAL_NIXPKGSREVISION=${LOCAL_NIXPKGSREVISION:0:${#REMOTE_NIXPKGSREVISION}}
 
 WORKFILE=~/.working-nixpkgs
@@ -129,6 +159,7 @@ else
 fi
 
 unset 'NIXCHANNELS["nixpkgs"]'
+unset 'FEEDS["nixpkgs"]'
 
 ## Compute the maximum length of channel name
 max_namelen=0
@@ -145,33 +176,33 @@ done
 # Add length of _remote_hash
 ((max_namelen = max_namelen + $(echo -n "_remote_hash" | wc -m)))
 
-ETAGDIR=~/.cache/nixchannels_etags
-mkdir -p $ETAGDIR
+HASHDIR=~/.cache/nixchannels_hash
+mkdir -p $HASHDIR
 echo ""
 echo "==============="
-for channame in "${!NIXCHANNELS[@]}"; do
-    url=${NIXCHANNELS[$channame]}
+for channame in "${!FEEDS[@]}"; do
+    url=${FEEDS[$channame]}
 
-    # 1. Read the existing ETag if it exists
-    tagfile=$ETAGDIR/${channame}_etag
-    LOCAL_ETAG=""
-    if [ -f "$tagfile" ]; then
-        LOCAL_ETAG=$(cat "$tagfile")
+    # 1. Read the existing hash if it exists
+    hashfile=$HASHDIR/${channame}_hash
+    LOCAL_HASH=""
+    if [ -f "$hashfile" ]; then
+        LOCAL_HASH=$(cat "$hashfile")
     fi
 
-    # 2. Get the remote etag of the channel url
-    REMOTE_ETAG=$(get_etag "$url")
+    # 2. Get the remote commit hash of the channel feed
+    REMOTE_HASH=$(get_atominfo "$url")
 
-    # 3. Compare the tags
-    if [[ "$LOCAL_ETAG" == "$REMOTE_ETAG" ]]; then
+    # 3. Compare the commit hashes
+    if [[ "$LOCAL_HASH" == "$REMOTE_HASH" ]]; then
         printf "%s=== Local package is up-to-date with %s channel ===%s\n" "${GREEN}${BOLD}" "$channame" "$ESC"
-        printf "%s==>%s  %-*s: %s\n" "$BLUE$BOLD" "$ESC" "$max_namelen" "${channame}_local_etag" "$LOCAL_ETAG"
-    elif [ -n "$REMOTE_ETAG" ]; then
+        printf "%s==>%s  %-*s: %s\n" "$BLUE$BOLD" "$ESC" "$max_namelen" "${channame}_local_hash" "$LOCAL_HASH"
+    elif [ -n "$REMOTE_HASH" ]; then
         printf "%s*** New package detected on %s channel ***%s\n" "$GREEN$BOLD" "$channame" "$ESC" >&"$OUTPUT"
-        printf "%s==>%s  %-*s: %s\n" "$BLUE$BOLD" "$ESC" "$max_namelen" "${channame}_local_etag" "$LOCAL_ETAG" >&"$OUTPUT"
-        printf "%s==>%s  %-*s: %s\n" "$BLUE$BOLD" "$RED$BOLD" "$max_namelen" "${channame}_remote_etag" "$REMOTE_ETAG$ESC" >&"$OUTPUT"
+        printf "%s==>%s  %-*s: %s\n" "$BLUE$BOLD" "$ESC" "$max_namelen" "${channame}_local_hash" "$LOCAL_HASH" >&"$OUTPUT"
+        printf "%s==>%s  %-*s: %s\n" "$BLUE$BOLD" "$RED$BOLD" "$max_namelen" "${channame}_remote_hash" "$REMOTE_HASH$ESC" >&"$OUTPUT"
     else
-        printf "%sCould not get ETag for %s from %s%s\n" "${BOLD}${RED}" "$channame" "$url" "${ESC}"
+        printf "%sCould not get commit hash for %s from %s%s\n" "${BOLD}${RED}" "$channame" "$url" "${ESC}"
     fi
 done
 
