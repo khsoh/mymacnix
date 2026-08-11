0ded13de79d9310171e7ef499ebc9affba52859b
K H Soh <hju37823@outlook.com>
2026-08-11 13:15:40 +0800

    Corrected method of determining nixpkgs revision

--- nixpkgs/darwin/configuration.nix
+++ nixpkgs/darwin/configuration.nix
@@ -43,7 +43,13 @@ let
 
   # 2. Extract the exact hash from the -I archive URL string
   urlMatch = builtins.match ".*archive/([0-9a-fA-F]{7,40})\\.tar\\.gz" activeNixpkgsPathStr;
-  currentRevision = if urlMatch != null then builtins.head urlMatch else null;
+  gitRevPath = "${activeNixpkgsPathStr}/.git-revision";
+  gitRevStr =
+    if builtins.pathExists gitRevPath then
+      builtins.readFile "${activeNixpkgsPathStr}/.git-revision"
+    else
+      null;
+  currentRevision = if urlMatch != null then builtins.head urlMatch else gitRevStr;
 in
 {
   # Replace with pkgs-pinned packages (the default)
